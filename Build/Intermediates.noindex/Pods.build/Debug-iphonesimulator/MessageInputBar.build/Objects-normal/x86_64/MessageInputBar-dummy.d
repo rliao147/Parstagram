28e47ff177239240c6e83f412c3feacd63db9457
rliao147 <ryanliao123@gmail.com>
2019-05-21 19:07:59 -0700

    Commited Uncommited Files

---
+++ Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/MessageInputBar.build/Objects-normal/x86_64/MessageInputBar-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/RyanLiao/Desktop/XCodeWorkspace/Parstagram/Pods/Target\ Support\ Files/MessageInputBar/MessageInputBar-dummy.m \
+  /Users/RyanLiao/Desktop/XCodeWorkspace/Parstagram/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/MessageInputBar.build/module.modulemap \
+  /Users/RyanLiao/Desktop/XCodeWorkspace/Parstagram/Pods/Target\ Support\ Files/MessageInputBar/MessageInputBar-prefix.pch
---
+++ Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/MessageInputBar.build/Objects-normal/x86_64/MessageInputBar_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/RyanLiao/Desktop/XCodeWorkspace/Parstagram/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/MessageInputBar.build/DerivedSources/MessageInputBar_vers.c
